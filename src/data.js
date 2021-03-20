edeab0c1b266d15b085054f2e31b7760b5e7672f
venkatesh dharmavarapu <venkatadharma1@gmail.com>
2021-03-20 14:02:02 +0530

    whole data changed

---
+++ src/data.js
@@ -0,0 +1,41 @@
+const reviews = [
+    {
+      id: 1,
+      name: 'Tarun Spartan',
+      job: 'web developer',
+      image:
+        'https://scontent.fhyd1-4.fna.fbcdn.net/v/t1.0-9/122330637_2770654983176795_7604211432092175115_o.jpg?_nc_cat=102&ccb=1-3&_nc_sid=174925&_nc_ohc=AtEmtapEOq8AX96t1RH&_nc_ht=scontent.fhyd1-4.fna&oh=c42104cc8d1c9e586fa0297d50669c55&oe=607CF68B',
+      text:
+        "hey there .. i am a senior developer in this company ... my colleaques are so cool .. manager and other senior developers are so encouraging ... ",
+    },
+    {
+      id: 2,
+      name: 'susmitha chari',
+      job: 'web designer',
+      image:
+        'https://scontent.fhyd1-2.fna.fbcdn.net/v/t1.0-9/141010418_2591093811036211_2772754920310658966_o.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=Ein6rtHYkn4AX9F5T4T&_nc_ht=scontent.fhyd1-2.fna&oh=6c283987c4a5da954587224d1636aa58&oe=607A7957',
+      text:
+        'i recently joined into the team & there i am receiving lot of supporting from juniors to seniors as well this ... was sooo amazing to say na ...',
+    },
+    {
+      id: 3,
+      name: 'venkatesh mano raja',
+      job: 'interns',
+      image:
+        'https://scontent.fhyd1-3.fna.fbcdn.net/v/t1.0-9/89654847_2549720435137986_6155859523073474560_o.jpg?_nc_cat=105&ccb=1-3&_nc_sid=730e14&_nc_ohc=hl38X69uHfEAX-vN5tE&_nc_ht=scontent.fhyd1-3.fna&oh=b591becb9210ba5fe3e799312e1692bd&oe=607B90A1',
+      text:
+        ' we are the inters ... recently joined here and the team was sooo supporting and the tasks are perfect for us to do & learn ...',
+    },
+    {
+      id: 4,
+      name: 'sri harsha',
+      job: '. Net',
+      image:
+        'https://scontent.fhyd1-2.fna.fbcdn.net/v/t1.0-9/89682083_2610006975911385_4519953114781974528_o.jpg?_nc_cat=107&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=LVB7Yf5V9YIAX_6fY11&_nc_ht=scontent.fhyd1-2.fna&oh=5567baaf2b143982ed5a08b699032b8f&oe=607CFDC4',
+      text:
+        ' came from a big company to here ... no mention for the previous company ... but a beautiful & supportive stafff here makes me more warmer ...',
+    },
+  ];
+  
+  export default reviews;
+  
